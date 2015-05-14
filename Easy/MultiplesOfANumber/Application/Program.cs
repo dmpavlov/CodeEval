520da10adc62bbc2675af8049c99db750a249dd3
Dmitry Pavlov <dmitry.pavlov@smartstepgroup.com>
2015-05-14 17:48:20 +0300

    Added solution for MultiplesOfANumber.

---
+++ Easy/MultiplesOfANumber/Application/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using System;
+
+namespace Application {
+	class MainClass {
+		public static void Main(string[] args) {
+			var multipleFinder = new MultipleFinder();
+			using (var reader = File.OpenText(args[0]))
+				while (!reader.EndOfStream) {
+					var line = reader.ReadLine();
+					if (line == null) continue;
+
+					var numbers = line.Split(',');
+					Console.WriteLine(multipleFinder.FindFor(int.Parse(numbers[0]), 
+					                                         int.Parse(numbers[1])));
+				}
+		}
+	}
+
+	public class MultipleFinder {
+		public int FindFor(int x, int n) {
+			if (x <= n) {
+				return n;
+			}
+
+			var result = n;
+			do {
+				result += n;
+			}
+			while (result < x);
+
+			return result;
+		}
+	}
+}
---
+++ Easy/MultiplesOfANumber/Tests/MultipleFinderShould.cs
@@ -0,0 +1,54 @@
+﻿using NUnit.Framework;
+using Application;
+using NUnit.Framework.SyntaxHelpers;
+
+namespace Tests {
+	[TestFixture]
+	public class MultipleFinderShould {
+		[Test]
+		public void ReturnZeroIfInputIsZero() {
+			var multipleFinder = new MultipleFinder();
+
+			var result = multipleFinder.FindFor(0, 0);
+
+			Assert.That(result, Is.EqualTo(0));
+		}
+
+		[Test]
+		public void ReturnXIfNIsEqualToX() {
+			var multipleFinder = new MultipleFinder();
+
+			var result = multipleFinder.FindFor(x: 2, n: 2);
+
+			Assert.That(result, Is.EqualTo(2));
+		}
+
+		[Test]
+		public void ReturnSmallestMultipleOfNGreaterThanX() {
+			var multipleFinder = new MultipleFinder();
+
+			var result = multipleFinder.FindFor(x: 3, n: 2);
+
+			Assert.That(result, Is.EqualTo(2 * 2));
+		}
+
+		[Test]
+		public void ReturnSmallestMultipleOfNIfXIs1635() {
+			var multipleFinder = new MultipleFinder();
+
+			var result = multipleFinder.FindFor(x: 1400, n: 512);
+
+			Assert.That(result, Is.EqualTo(512 * 3));
+		}
+
+		[Test]
+		public void ReturnNIfNIsAlreadyGreaterThanX() {
+			var multipleFinder = new MultipleFinder();
+
+			var result = multipleFinder.FindFor(x: 1, n: 2);
+
+			Assert.That(result, Is.EqualTo(2));
+		}
+	}
+}
+
